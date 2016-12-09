3824ec1d505a4cf0c4d370a593823c541bf85050
yuanlw <myzone101@126.com>
2016-12-09 16:40:21 +0800

    first

---
+++ src/java/main/SpiderMan.java
@@ -0,0 +1,18 @@
+package main;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yuanliwei@le.com
+ * Date: 2016/12/9
+ * Time: 15:51
+ * DESCRIPTION:
+ */
+public class SpiderMan {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello spider man!!");
+    }
+
+
+}
